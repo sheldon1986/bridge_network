fa80f47dfa25e1b43a54fd1dcf8759d8175f8af5
sheldon <sheldon@code-post.com>
2018-06-12 06:43:10 +0000

    update

--- br.sh
+++ br.sh
@@ -29,12 +29,26 @@ package_checker netstat net-tools
 package_checker brctl bridge-utils
 
 interface=`ip addr |grep $IP |awk -F " " '{print $NF}'`
-GW=`netstat -rn  |grep $interface |awk -F " " '{print $2}' |grep -v 0.0.0.0`
+#GW=`netstat -rn  |grep $interface |awk -F " " '{print $2}' |grep -v 0.0.0.0`
+GW=192.0.0.3
 bridge=br-metrom
 bridge_chk=`brctl show |grep $bridge |wc -l`
 gw_chk_file="/tmp/gateway.txt"
 interface_addr_file="/tmp/interface.txt"
-NM=`ifconfig |grep $IP |awk '{print $4}'`
+ip_route=`ip route |grep $interface |grep -v default |awk '{print $1}' |awk -F "/" '{print $2}'`
+ip_route_BAK=`ip route |grep $interface|grep -v default |awk '{print $1}' |awk -F "/" '{print $2}'`
+
+
+cdr2mask ()
+{
+   # Number of args to shift, 255..255, first non-255 byte, zeroes
+   set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+   [ $1 -gt 1 ] && shift $1 || shift
+   echo ${1-0}.${2-0}.${3-0}.${4-0}
+}
+
+NM=`cdr2mask $ip_route`
+NM_BAK=`cdr2mask $ip_route_BAK`
 
 
 if ! ping $GW -c 1 ;then
@@ -42,11 +56,11 @@ if ! ping $GW -c 1 ;then
 else
         echo "Bridge network configuration"
 	echo "$interface" > $interface_addr_file
-        modprobe bridge;
-        brctl addbr $bridge;
-        brctl addif $bridge $interface;
-        ifconfig $interface 0.0.0.0;
-        ifconfig $bridge $IP up netmask $NM;
+        modprobe bridge
+        brctl addbr $bridge
+        brctl addif $bridge $interface
+        ifconfig $interface 0.0.0.0
+        ifconfig $bridge $IP up netmask $NM
         route add -net default gw $GW
 	echo "$GW" > $gw_chk_file
 fi
@@ -56,9 +70,9 @@ if ping $GW -c 1 ;then
 	echo "complete"
 elif [[ $bridge_chk == 1 ]];then
         echo "Bridge Configuration rollback"
-	ip link set $bridge down;
-        brctl delbr $bridge;
-	ifconfig $(cat $interface_addr_file) $IP netmask $NM;
+	ip link set $bridge down
+        brctl delbr $bridge
+	ifconfig $(cat $interface_addr_file) $IP netmask $NM_BAK
 	route add -net default gw $(cat $gw_chk_file)
 else
 	exit 1
